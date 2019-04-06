6bf2ce663cf50341f67ffa2c458c5d95f00f0070
Ansh Gujral <anshgujral1997@gmail.com>
2019-04-06 16:24:44 +0530

    initiat hom screen
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ animation/lib/src/screns/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {}
+}
