a91518c167018d8a74110f29b8b5506be52f7162
Ansh Gujral <anshgujral1997@gmail.com>
2019-04-07 15:50:11 +0530

    create fetch top ids functionality
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ news/lib/src/resources/news_api_provider.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+
+class NewsApiProvider {
+  Client client = Client();
+
+  fetchTopIds() async {
+    final response = await client
+        .get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    final ids = json.decode(response.body);
+    return ids;
+  }
+
+  fetchItem() {}
+}
