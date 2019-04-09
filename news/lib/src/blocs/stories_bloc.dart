ea7bb3b76d804398e1b5368020133081951fcba4
Ansh Gujral <anshgujral1997@gmail.com>
2019-04-09 22:34:45 +0530

    create bloc
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

---
+++ news/lib/src/blocs/stories_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>();
+
+  //Get To streams
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
+}
